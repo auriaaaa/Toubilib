f7b60d8452a7e093563e5f0fe1862cb146289ee0
Auriane Guyot <aurianeguyot70@gmail.com>
2026-09-21 18:49:04 +0200

    Création du bootstrap

---
+++ app/config/bootstrap.php
@@ -0,0 +1,52 @@
+<?php
+
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+use toubilib\adapters\config\ContainerConfig;
+use toubilib\adapters\controllers\middlewares\Cors;
+use toubilib\adapters\controllers\actions\AnnulerRendezVousAction;
+
+$dotenv = \Dotenv\Dotenv::createImmutable(
+    __DIR__ . '/../../',
+    'toubilib.env'
+);
+$dotenv->load();
+
+$container = ContainerConfig::build();
+
+$app = AppFactory::createFromContainer($container);
+$app->addBodyParsingMiddleware();
+$app->add(Cors::class);
+$app->addRoutingMiddleware();
+
+$errorMiddleware = $app->addErrorMiddleware(true, false, false);
+$errorHandler = $errorMiddleware->getDefaultErrorHandler();
+$errorHandler->forceContentType('application/json');
+$errorHandler->registerErrorRenderer(
+    'application/json',
+    function ($exception, $displayErrorDetails) {
+        return json_encode([
+            'message' => $exception->getMessage()
+        ]);
+    }
+);
+
+$app->group('/rdv', function (\Slim\Routing\RouteCollectorProxy $group) {
+
+    // Annuler un rendez-vous
+    $group->post('/{id}/annuler', AnnulerRendezVousAction::class )->setName('AnnulerRendezVous');
+
+});
+
+$app->options(
+    '/{routes:.+}',
+    function (Request $request, Response $response): Response {
+        return $response;
+    }
+);
+
+$routeParser = $app->getRouteCollector()->getRouteParser();
+
+return $app;
