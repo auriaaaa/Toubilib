305a0ace4380fcefb493e275104f3b240e66c0f1
Auriane Guyot <aurianeguyot70@gmail.com>
2026-09-22 17:14:29 +0200

    Ajout des définitions concernant les praticiens

--- app/src/adapters/config/ContainerConfig.php
+++ app/src/adapters/config/ContainerConfig.php
@@ -5,11 +5,15 @@ namespace toubilib\adapters\config;
 use DI\ContainerBuilder;
 use Psr\Container\ContainerInterface;
 
+use toubilib\adapters\persistence\RendezVousRepository;
+use toubilib\application\ports\spi\RendezVousRepositoryInterface;
 use toubilib\application\usecases\ServiceRendezVous;
 use toubilib\application\ports\api\ServiceRendezVousInterface;
 
-use toubilib\adapters\persistence\RendezVousRepository;
-use toubilib\application\ports\spi\RendezVousRepositoryInterface;
+use toubilib\adapters\persistence\PraticienRepository;
+use toubilib\application\ports\spi\PraticienRepositoryInterface;
+use toubilib\application\usecases\ServicePraticien;
+use toubilib\application\ports\api\ServicePraticienInterface;
 
 final class ContainerConfig
 {
@@ -31,6 +35,17 @@ final class ContainerConfig
                         \DI\get('toubilib.pdo')
                     ),
 
+           ServicePraticienInterface::class =>
+                \DI\create(ServicePraticien::class)
+                    ->constructor(
+                        \DI\get(PraticienRepositoryInterface::class)
+                    ),
+
+            PraticienRepositoryInterface::class =>
+                \DI\create(PraticienRepository::class)
+                    ->constructor(
+                        \DI\get('toubilib.pdo')
+                    ),
 
             'toubilib.pdo' => function (ContainerInterface $c) {
 
